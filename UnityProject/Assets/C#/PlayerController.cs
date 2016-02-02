4302a858f765db9a38f9bba711a422b008aa9889
esther5576 <esther.berges@hotmail.com>
2016-02-02 13:34:31 +0100

    Base Project With Netwok
    Hub
    Game
    Player instantiation

---
+++ UnityProject/Assets/C#/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isLocalPlayer)
+		{
+			if(Input.GetKey(KeyCode.Q))
+			{
+				transform.position -= new Vector3(0.01f,0,0);
+			}
+			if(Input.GetKey(KeyCode.D))
+			{
+				transform.position += new Vector3(0.01f,0,0);
+			}
+		}
+	}
+}
